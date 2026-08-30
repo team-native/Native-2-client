06ea4364bbcb28155aa42e687e1f91a7ecb2d15f
(i1anzu) <(s26040@gsm.hs.kr)>
2026-08-31 02:27:03 +0900

    chore: Vite 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+});
